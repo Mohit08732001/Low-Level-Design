95c9fcabcd716921a6d8e27c88301aa4cbc9c196
Mohit08732001 <mohit0873.cse19@chitkara.edu.in>
2024-06-09 12:37:41 +0530

    classes and objects

---
+++ LLD1.py
@@ -0,0 +1,18 @@
+"""
+Programming Paradigms In Language
+Procedural, Object Oriented, Functional
+
+Since we are focusing on Python, Python uses OOP
+
+OOP is basically real world entites called as classes and instance of those created are called as objects.
+OOP is based on abstraction
+Pillars of OOP: Encapsulation, Polymorphism, Inheritance
+"""
+
+# Created a class called as House
+class House:
+    def __init__(self) -> None:
+        print('Constructor of house is called')
+
+# Created a instance of class which is called object
+h = House()
