83acc2e5fcaed2b2b4a0db771da3fc0f27d1bb68
Mohit08732001 <mohit0873.cse19@chitkara.edu.in>
2024-06-09 15:34:50 +0530

    open closed principle

--- LLD4.py
+++ LLD4.py
@@ -44,3 +44,54 @@ class ZipFileManager:
         pass
 
 # Here we solved the problem by dividing the class, into two classes which both have only one reason to change now
+
+
+# O -> Open Closed Principle
+# This principle states that a class should be open for extension but closed for modification
+
+from math import pi
+class Shape:
+    def __init__(self, shape_type, **kwargs) -> None:
+        self.shape_type = shape_type
+        if shape_type == 'circle':
+            self.radius = kwargs.get('radius')
+        elif shape_type == 'rectangle':
+            self.height = kwargs.get('height')
+            self.width = kwargs.get('width')
+    
+    def calculate_area(self):
+        if self.shape_type == 'circle':
+            return pi*self.radius*self.radius
+        elif self.shape_type == 'rectangle':
+            return self.height*self.width
+
+# This class is violating OCP, because say in future we have a new shape_type, then we need to change all methods in the class
+
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    def __init__(self, shape_type) -> None:
+        self.shape_type = shape_type
+    
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, height, width) -> None:
+        super().__init__('rectangle')
+        self.height = height
+        self.width = width
+    
+    def calculate_area(self):
+        return self.height * self.width
+
+class Circle(Shape):
+    def __init__(self, radius) -> None:
+        super().__init__('circle')
+        self.radius = radius
+    
+    def calculate_area(self):
+        return pi * self.radius * self.radius
+
+# Here we solved the issue of OCP, by creating an abstract class shape, which will never be modified, now if new shape comes we can create a new
+# class for that shape-type which will implement Shape abstarct class
